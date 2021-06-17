941c66aafe5714efefa7bd991e43d266345296ce
Urikke <guilhermenogueira.design@gmail.com>
2021-06-17 19:59:51 -0300

    Adicionando programa dos numeros

--- DigitalInnovationOne/Program.cs
+++ DigitalInnovationOne/Program.cs
@@ -6,7 +6,12 @@ namespace DigitalInnovationOne
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numeroVezes = 5;
+
+            for(int i=0; i <= numeroVezes; i++)
+            {
+                Console.WriteLine($"O numero é {i}");
+            }
         }
     }
 }
