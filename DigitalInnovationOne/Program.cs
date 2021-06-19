4760f28587287af2a0fbda51a9deb4cf25815c4f
Urikke <guilhermenogueira.design@gmail.com>
2021-06-18 21:04:28 -0300

    Criando e lendo o arquivo de Texto

--- DigitalInnovationOne/Program.cs
+++ DigitalInnovationOne/Program.cs
@@ -6,11 +6,51 @@ namespace DigitalInnovationOne
     {
         static void Main(string[] args)
         {
-            int numeroVezes = 5;
-
-            for(int i=0; i <= numeroVezes; i++)
+            using(System.IO.TextWriter t =  System.IO.File.CreateText("TextoTeste.txt"))
             {
-                Console.WriteLine($"O numero é {i}");
+                t.WriteLine("Linha 1");
+                t.WriteLine("Linha 2");
+                t.WriteLine("Linha 3");   
+            }
+
+            string teste = System.IO.File.ReadAllText("TextoTeste.txt");
+
+            Console.WriteLine(teste);
+        }
+
+        static void TryCatchFinallyThrow(string[] args)
+        {
+            double Dividir(double x, double y)
+            {
+                if (y == 0)
+                    throw new DivideByZeroException();
+                
+                return x / y;
+            }
+
+            try
+            {
+                if(args.Length != 2)
+                    throw new InvalidOperationException("Informe dois números!");
+
+                double x = double.Parse(args[0]);
+                double y = double.Parse(args[1]);
+                Console.WriteLine(Dividir(x,y));
+            }
+            
+            catch(InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            catch(Exception e)
+            {
+                Console.WriteLine($"Erro Genérico: {e.Message}");
+            }
+
+            finally
+            {
+                Console.WriteLine("Até breve!");
             }
         }
     }
