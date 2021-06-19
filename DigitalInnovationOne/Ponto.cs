995af17049decd162c286279ad457d7f8883ed23
Urikke <guilhermenogueira.design@gmail.com>
2021-06-19 10:11:53 -0300

    nova alteração classes

--- DigitalInnovationOne/Ponto.cs
+++ DigitalInnovationOne/Ponto.cs
@@ -4,10 +4,26 @@ namespace DigitalInnovationOne
     {
         public int x;
         public int y;
+        private int distancia;
                 public Ponto(int x, int y)
         {
             this.x = x;
             this.y = y;
+        }
+
+        public void CalcularDistancia()
+        {
+
+        }
+
+        protected void CalcularDistancia2()
+        {
+            
+        }
+
+        private void CalcularDistancia3()
+        {
+            
         }
     }
 } 
